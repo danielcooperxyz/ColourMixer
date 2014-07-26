d55e791af4abf03f8633d03f2dd3d38704b9ee31
Daniel Cooper <cooper.daniel45@gmail.com>
2014-07-26 18:55:34 +0100

    Colours now display on selection from the dropdown. Added the result display box. Lightly styled the header and background.

--- ChicMouldings.ColourMixer.Website/ChicMouldings.ColourMixer.Website/App_Start/BundleConfig.cs
+++ ChicMouldings.ColourMixer.Website/ChicMouldings.ColourMixer.Website/App_Start/BundleConfig.cs
@@ -13,7 +13,8 @@ namespace ChicMouldings.ColourMixer.Website.App_Start
         {
             bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
                         "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/ColourMixer.js"));
+                        "~/Scripts/ColourMixer.js",
+                        "~/Scripts/RGBColor.js"));
 
             bundles.Add(new StyleBundle("~/bundles/styles").Include(
                         "~/Styles/*.css"));
--- ChicMouldings.ColourMixer.Website/ChicMouldings.ColourMixer.Website/Scripts/ColourMixer.js
+++ ChicMouldings.ColourMixer.Website/ChicMouldings.ColourMixer.Website/Scripts/ColourMixer.js
@@ -1,45 +1,44 @@
 ﻿
-function populateDropdowns()
+function populateDropdowns(dropdowns)
 {
-    var index, data, dropdowns, option, newOption;
+    var index, data, option, newOption;
 
     data =
     [
         { text: 'Select a Color', value: '0' },
-        { text: 'Antibes Green', value: 'rgb(95,173,72)' },
-        { text: 'Antoinette', value: 'rgb(243,218,221)' },
-        { text: 'Arles', value: 'rgb(231,174,69)' },
-        { text: 'Aubusson Blue', value: 'rgb(51,90,133)' },
-        { text: 'Barcelona Orange', value: 'rgb(230,134,76)' },
-        { text: 'Chateau Grey', value: 'rgb(110,117,75)' },
-        { text: 'Coco', value: 'rgb(141,128,112)' },
-        { text: 'Country Grey', value: 'rgb(198,195,176)' },
-        { text: 'Cream', value: 'rgb(234,230,192)' },
-        { text: 'Duck Egg Blue', value: 'rgb(127,163,159)' },
-        { text: 'Emile', value: 'rgb(142,124,136)' },
-        { text: 'Emperors Silk', value: 'rgb(170,11,39)' },
-        { text: 'English Yellow', value: 'rgb(254,234,124)' },
-        { text: 'Florence', value: 'rgb(3,98,102)' },
-        { text: 'French Linen', value: 'rgb(171,163,144)' },
-        { text: 'Graphite', value: 'rgb(28,28,28)' },
-        { text: 'Greek Blue', value: 'rgb(38,79,143)' },
-        { text: 'Henrietta', value: 'rgb(205,145,181)' },
-        { text: 'Louis Blue', value: 'rgb(191,206,237)' },
-        { text: 'Napoleonic Blue', value: 'rgb(13,68,132)' },
-        { text: 'Old Ochre', value: 'rgb(212,207,175)' },
-        { text: 'Old White', value: 'rgb(255,249,225)' },
-        { text: 'Old Violet', value: 'rgb(70,78,114)' },
-        { text: 'Olive', value: 'rgb(65,61,32)' },
-        { text: 'Paloma', value: 'rgb(168,162,162)' },
-        { text: 'Paris Grey', value: 'rgb(205,205,205)' },
-        { text: 'Primer Red', value: 'rgb(109,48,30)' },
-        { text: 'Provence', value: 'rgb(110,162,173)' },
-        { text: 'Pure White', value: 'rgb(255,255,255)' },
-        { text: 'Scandinavian Pink', value: 'rgb(206,116,108)' },
-        { text: 'Versailles', value: 'rgb(206,204,130)' }
+        { text: 'Antibes Green', value: '95,173,72' },
+        { text: 'Antoinette', value: '243,218,221' },
+        { text: 'Arles', value: '231,174,69' },
+        { text: 'Aubusson Blue', value: '51,90,133' },
+        { text: 'Barcelona Orange', value: '230,134,76' },
+        { text: 'Chateau Grey', value: '110,117,75' },
+        { text: 'Coco', value: '141,128,112' },
+        { text: 'Country Grey', value: '198,195,176' },
+        { text: 'Cream', value: '234,230,192' },
+        { text: 'Duck Egg Blue', value: '127,163,159' },
+        { text: 'Emile', value: '142,124,136' },
+        { text: 'Emperors Silk', value: '170,11,39' },
+        { text: 'English Yellow', value: '254,234,124' },
+        { text: 'Florence', value: '3,98,102' },
+        { text: 'French Linen', value: '171,163,144' },
+        { text: 'Graphite', value: '28,28,28' },
+        { text: 'Greek Blue', value: '38,79,143' },
+        { text: 'Henrietta', value: '205,145,181' },
+        { text: 'Louis Blue', value: '191,206,237' },
+        { text: 'Napoleonic Blue', value: '13,68,132' },
+        { text: 'Old Ochre', value: '212,207,175' },
+        { text: 'Old White', value: '255,249,225' },
+        { text: 'Old Violet', value: '70,78,114' },
+        { text: 'Olive', value: '65,61,32' },
+        { text: 'Paloma', value: '168,162,162' },
+        { text: 'Paris Grey', value: '205,205,205' },
+        { text: 'Primer Red', value: '109,48,30' },
+        { text: 'Provence', value: '110,162,173' },
+        { text: 'Pure White', value: '255,255,255' },
+        { text: 'Scandinavian Pink', value: '206,116,108' },
+        { text: 'Versailles', value: '206,204,130' }
     ];
 
-    dropdowns = $('.Dropdown');
 
     for (index = 0; index < data.length; index++)
     {
@@ -52,7 +51,24 @@ function populateDropdowns()
     }
 }
 
+function colourSelected()
+{
+    var selectedColour, colour, displayBox;
+    
+    selectedColour = $(this).val();
+
+    colour = new RGBColor('rgb(' + selectedColour + ')');
+
+    displayBox = $(this).siblings('.PaintDisplay');
+
+    displayBox.css('background', colour.toHex());
+}
+
 $(document).ready(function ()
 {
-    populateDropdowns();
+    var dropdowns = $('.Dropdown');
+
+    populateDropdowns(dropdowns);
+
+    dropdowns.change(colourSelected);
 });
