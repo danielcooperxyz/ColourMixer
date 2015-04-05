0f0d553513c2f54384582c984ace7f5db67a3b26
Daniel Cooper <cooper.daniel45@gmail.com>
2015-04-05 15:25:53 +0100

    Removed jquery Added support for multiple paint sets

--- ChicMouldings.ColourMixer.Website/ChicMouldings.ColourMixer.Website/Scripts/ColourMixer.js
+++ ChicMouldings.ColourMixer.Website/ChicMouldings.ColourMixer.Website/Scripts/ColourMixer.js
@@ -1,69 +1,102 @@
 ﻿/*! ColourMixer.js
 * Copyright 2014 Daniel Cooper
 */
-function populateDropdowns(dropdowns) {
-    var index, data, option, newOption;
-
-    data =
+var data =
         [
-            { text: 'Select a Color', value: '0' },
-            { text: 'Antibes Green', value: '95,173,72' },
-            { text: 'Antoinette', value: '243,218,221' },
-            { text: 'Arles', value: '231,174,69' },
-            { text: 'Aubusson Blue', value: '51,90,133' },
-            { text: 'Barcelona Orange', value: '230,134,76' },
-            { text: 'Burgundy', value: '94,8,15' },
-            { text: 'Chateau Grey', value: '110,117,75' },
-            { text: 'Coco', value: '141,128,112' },
-            { text: 'Country Grey', value: '198,195,176' },
-            { text: 'Cream', value: '234,230,192' },
-            { text: 'Duck Egg Blue', value: '127,163,159' },
-            { text: 'Emile', value: '142,124,136' },
-            { text: 'Emperors Silk', value: '170,11,39' },
-            { text: 'English Yellow', value: '254,234,124' },
-            { text: 'Florence', value: '3,98,102' },
-            { text: 'French Linen', value: '171,163,144' },
-            { text: 'Graphite', value: '28,28,28' },
-            { text: 'Greek Blue', value: '38,79,143' },
-            { text: 'Henrietta', value: '205,145,181' },
-            { text: 'Louis Blue', value: '191,206,237' },
-            { text: 'Napoleonic Blue', value: '13,68,132' },
-            { text: 'Old Ochre', value: '212,207,175' },
-            { text: 'Old White', value: '255,249,225' },
-            { text: 'Old Violet', value: '70,78,114' },
-            { text: 'Olive', value: '65,61,32' },
-            { text: 'Paloma', value: '168,162,162' },
-            { text: 'Paris Grey', value: '205,205,205' },
-            { text: 'Primer Red', value: '109,48,30' },
-            { text: 'Provence', value: '110,162,173' },
-            { text: 'Pure', value: '255,255,255' },
-            { text: 'Scandinavian Pink', value: '206,116,108' },
-            { text: 'Versailles', value: '206,204,130' }
+            {
+                name: 'Annie Sloane',
+                colours:
+                    [
+                        { text: 'Select a Color', value: '0' },
+                        { text: 'Antibes Green', value: '95,173,72' },
+                        { text: 'Antoinette', value: '243,218,221' },
+                        { text: 'Arles', value: '231,174,69' },
+                        { text: 'Aubusson Blue', value: '51,90,133' },
+                        { text: 'Barcelona Orange', value: '230,134,76' },
+                        { text: 'Burgundy', value: '94,8,15' },
+                        { text: 'Chateau Grey', value: '110,117,75' },
+                        { text: 'Coco', value: '141,128,112' },
+                        { text: 'Country Grey', value: '198,195,176' },
+                        { text: 'Cream', value: '234,230,192' },
+                        { text: 'Duck Egg Blue', value: '127,163,159' },
+                        { text: 'Emile', value: '142,124,136' },
+                        { text: 'Emperors Silk', value: '170,11,39' },
+                        { text: 'English Yellow', value: '254,234,124' },
+                        { text: 'Florence', value: '3,98,102' },
+                        { text: 'French Linen', value: '171,163,144' },
+                        { text: 'Graphite', value: '28,28,28' },
+                        { text: 'Greek Blue', value: '38,79,143' },
+                        { text: 'Henrietta', value: '205,145,181' },
+                        { text: 'Louis Blue', value: '191,206,237' },
+                        { text: 'Napoleonic Blue', value: '13,68,132' },
+                        { text: 'Old Ochre', value: '212,207,175' },
+                        { text: 'Old White', value: '255,249,225' },
+                        { text: 'Old Violet', value: '70,78,114' },
+                        { text: 'Olive', value: '65,61,32' },
+                        { text: 'Paloma', value: '168,162,162' },
+                        { text: 'Paris Grey', value: '205,205,205' },
+                        { text: 'Primer Red', value: '109,48,30' },
+                        { text: 'Provence', value: '110,162,173' },
+                        { text: 'Pure', value: '255,255,255' },
+                        { text: 'Scandinavian Pink', value: '206,116,108' },
+                        { text: 'Versailles', value: '206,204,130' }
+                    ]
+            }, {
+                name: 'New',
+                colours:
+                    [
+                        { text: 'Select a Color', value: '0' },
+                        { text: 'Antibes Green', value: '95,173,72' }
+                    ]
+            }
         ];
 
+function getHex(rgb) {
+    return new RGBColor('rgb(' + rgb + ')').toHex();
+}
 
-    for (index = 0; index < data.length; index++) {
-        option = data[index];
+function updateColourDropdowns() {
+    var dropdowns, i, j, dropdown, typeId, colours, option, newOption;
 
-        newOption = '<option value="' + option.value;
-        newOption += '">' + option.text + '</option>';
+    dropdowns = document.getElementsByClassName('PaintDropdown');
 
-        dropdowns.append(newOption);
+    for (i = 0; i < dropdowns.length; i++) {
+        dropdown = dropdowns[i];
+        dropdown.textContent = '';
+
+        typeId = document.getElementById('paintTypes').value;
+
+        colours = data[typeId].colours;
+
+        for (j = 0; j < colours.length; j++) {
+            option = colours[j];
+
+            newOption = document.createElement('option');
+            newOption.setAttribute('value', option.value);
+            newOption.appendChild(document.createTextNode(option.text));
+
+            dropdown.appendChild(newOption);
+        }
     }
 }
 
-function updateSelectedColour() {
-    var selectedColour, colour, displayBox;
+function populateColourTypes() {
+    var dropdown, index, type, newOption;
 
-    selectedColour = $(this).val();
+    dropdown = document.getElementById('paintTypes');
 
-    colour = new RGBColor('rgb(' + selectedColour + ')');
+    for (index = 0; index < data.length; index++) {
 
-    displayBox = $(this).parents('.Container').find('.PaintDisplay');
+        type = data[index];
 
-    displayBox.css('background', colour.toHex());
+        newOption = document.createElement('option');
+        newOption.setAttribute('value', index);
+        newOption.appendChild(document.createTextNode(type.name));
 
-    return false;
+        dropdown.appendChild(newOption);
+    }
+
+    dropdown.onclick = updateColourDropdowns;
 }
 
 function calculateTotals(array, parts, totalParts) {
@@ -81,22 +114,20 @@ function calculateTotals(array, parts, totalParts) {
 }
 
 function displayRecipe(colours, parts) {
-    var index = 0, recipeText = "<ul>", recipe = $('#recipe');
+    var index, recipeText, recipeContainer;
+
+    recipeText = document.createElement('ul');
+    recipeContainer = document.getElementById('recipe');
 
     for (index = 0; index < colours.length; index++) {
         if (parts[index] > 0) {
-            recipeText += "<li>" + parts[index] + " x "
-                 + colours[index].text() + "</li>";
+            recipeText.appendChild(document.createElement('li')
+                .appendChild(document.createTextNode(parts[index] +
+                    " x " + colours[index].text())));
         }
     }
 
-    recipeText += "</ul>";
-
-    recipe.html(recipeText);
-
-    if (recipe.text().length > 0) {
-        recipe.addClass('Populated');
-    }
+    recipeContainer.innerHtml = recipeText;
 }
 
 function mixColours() {
@@ -106,22 +137,22 @@ function mixColours() {
 
     colours =
         [
-            $('#paintOneValue option:selected'),
-            $('#paintTwoValue option:selected'),
-            $('#paintThreeValue option:selected'),
-            $('#paintFourValue option:selected')
+            document.getElementById('paintOneValue').value,
+            document.getElementById('paintTwoValue').value,
+            document.getElementById('paintThreeValue').value,
+            document.getElementById('paintFourValue').value
         ];
 
     parts =
         [
-            $('#paintOneParts').val(),
-            $('#paintTwoParts').val(),
-            $('#paintThreeParts').val(),
-            $('#paintFourParts').val()
+            document.getElementById('paintOneParts').value,
+            document.getElementById('paintTwoParts').value,
+            document.getElementById('paintThreeParts').value,
+            document.getElementById('paintFourParts').value
         ];
 
     for (index = 0; index < colours.length; index++) {
-        if (colours[index].val() === "0") {
+        if (colours[index] === "0") {
             parts[index] = 0;
         } else {
             if (parts[index].length === 0 || parts[index] === 0) {
@@ -131,7 +162,7 @@ function mixColours() {
 
         totalParts += parseInt(parts[index], 0);
 
-        temp = new RGBColor("rgb(" + colours[index].val() + ")");
+        temp = new RGBColor("rgb(" + colours[index] + ")");
 
         red.push(temp.r);
         green.push(temp.g);
@@ -142,10 +173,11 @@ function mixColours() {
     totals.push(calculateTotals(green, parts, totalParts));
     totals.push(calculateTotals(blue, parts, totalParts));
 
-    resultColour = new RGBColor("rgb(" + totals.join(',') + ")").toHex();
+    resultColour = getHex(totals.join(','));
 
     if (resultColour !== '#000000') {
-        $('#paintResult').css('background', resultColour);
+        document.getElementById('paintResult')
+            .setAttribute('style', 'background-color:' + resultColour);
     }
 
     displayRecipe(colours, parts);
@@ -153,41 +185,50 @@ function mixColours() {
     return false;
 }
 
-function reset() {
-    var dropdowns = $('.Dropdown'),
-        recipe = $('#recipe');
+function updateSelectedColour() {
+    var container, displayBox;
 
-    dropdowns.children('option').prop('selected', '');
-    $('.Spinner').val('');
+    container = this.closest('.Container');
+       
+    displayBox = container.querySelector('.PaintDisplay');
 
-    dropdowns.selectmenu('refresh');
-    $('.ui-selectmenu-button, .ui-spinner').css("width", "100%");
+    displayBox.setAttribute('style', 'background: ' + getHex(this.value));
 
-    $('.PaintDisplay, #paintResult').css('background-color', "#FFFFFF");
-    recipe.html('');
-    recipe.removeClass('Populated');
+    if (container.querySelector('.Spinner').value > 0) {
+        mixColours();
+    }
+
+    return false;
 }
 
-$(document).ready(function () {
-    var dropdowns = $('.Dropdown'),
-        spinners = $('.Spinner'),
-        mixButton = $('#mixButton'),
-        resetButton = $('#resetButton');
+function reset() {
+    //var dropdowns, recipe;
 
-    populateDropdowns(dropdowns);
+    //dropdowns.children('option').prop('selected', '');
+    //$('.Spinner').val('');
 
-    dropdowns.selectmenu({ change: updateSelectedColour });
+    //dropdowns.selectmenu('refresh');
+    //$('.ui-selectmenu-button, .ui-spinner').css("width", "100%");
 
-    spinners.spinner(
-        {
-            min: 0,
-            numberFormat: "C"
-        }
-    );
+    //('.PaintDisplay, #paintResult').css('background-color', "#FFFFFF");
+    //recipe.html('');
+    //recipe.removeClass('Populated');
+}
 
-    $('.ui-selectmenu-button, .ui-spinner, .ui-spinner-input').css("width", "100%");
+function setDropdownListeners() {
 
-    mixButton.click(mixColours);
+    var index, dropdowns = document.getElementsByClassName('PaintDropdown');
 
-    resetButton.click(reset);
-});
+    for (index = 0; index < dropdowns.length; index++) {
+        dropdowns[index].onchange = updateSelectedColour;
+    }
+}
+
+window.onload = function () {
+
+    populateColourTypes();
+
+    updateColourDropdowns();
+
+    setDropdownListeners();
+};
